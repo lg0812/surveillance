5f1d6fa9b695aab1ffff1cc1786b855602da7f5c
LG0812 <2356581512@qq.com>
2017-11-07 15:00:19 +0800

    none

--- src/app/supervise/sidebar/sidebar.component.ts
+++ src/app/supervise/sidebar/sidebar.component.ts
@@ -9,10 +9,10 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SideBarComponent implements OnInit {
-  menus: object[] = [{
+  menus: any[] = [{
     "menuImg": "../../../assets/images/setting.png", menuName: "abc", subs: [
-      {"menuImg": "../../../assets/images/setting.png", menuName: "subabc", active: false, path: "/supervise/order"},
-      {"menuImg": "../../../assets/images/setting.png", menuName: "subabc", active: true, path: "/supervise/service"}
+      {"menuImg": "../../../assets/images/setting.png", menuName: "subabc", active: true, path: "/supervise/order"},
+      {"menuImg": "../../../assets/images/setting.png", menuName: "subabc", active: false, path: "/supervise/service"}
     ],
     active: true
   }, {
@@ -26,7 +26,21 @@ export class SideBarComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  getKeys(obj) {
-    return Object.keys(obj);
+
+  toggleMenu(ev, index, subIndex) {
+    if (subIndex != undefined) {
+      for (let t = 0; t < this.menus.length; t++) {
+        for (const sub of this.menus[t].subs) {
+          sub.active = false;
+        }
+      }
+      this.menus[index].subs[subIndex].active = true;
+    } else {
+      for (let t = 0; t < this.menus.length; t++) {
+        if (t != index)
+          this.menus[t].active = false;
+      }
+      this.menus[index].active = !this.menus[index].active;
+    }
   }
 }
